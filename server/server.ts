10ce24ca20167a9fb84be159695c18de9ed36ee8
Hanshika Shanvi <hshanvi30@gmail.com>
2026-02-28 03:02:56 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -25,7 +25,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "https://ai-thumbnail-generator-one.vercel.app",
     ],
     credentials: true,
   }),
